09ea97347c801264dca730d93b490a5ad493abe3
Daniel Chernyshev <droque123@gmail.com>
2020-04-18 20:42:40 +0300

    Updates

---
+++ precache-manifest.f61c5b7a87e9201e08a8df27643764b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97076987635947e14701ea47a2c440fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f907c62fcace362ac2bd",
+    "url": "/static/css/main.16339a73.chunk.css"
+  },
+  {
+    "revision": "127615e3c9a6b6e107e1",
+    "url": "/static/js/2.1d70f6d0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1d70f6d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f907c62fcace362ac2bd",
+    "url": "/static/js/main.db1ed1da.chunk.js"
+  },
+  {
+    "revision": "b8195dc097a169901709",
+    "url": "/static/js/runtime-main.cfcd4d48.js"
+  },
+  {
+    "revision": "b1f95b6cda8cc5265e4323baaddbad3c",
+    "url": "/static/media/table1.b1f95b6c.jpg"
+  },
+  {
+    "revision": "260ce06550a419fb522df44c78b31934",
+    "url": "/static/media/table2.260ce065.jpg"
+  }
+]);
