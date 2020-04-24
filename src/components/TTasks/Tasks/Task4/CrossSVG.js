c1e03dc33f16444b1483f88b776fa97a761cc8be
Daniel Chernyshev <droque123@gmail.com>
2020-04-24 12:12:34 +0300

    Added Svg and throttle

---
+++ src/components/TTasks/Tasks/Task4/CrossSVG.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+        <div className="cross-svg__container">
+            <svg className="cross-svg" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path className="cross-svg_path" d="M13.9844 1.42188L8.40625 7L13.9844 12.5781L12.5781 13.9844L7 8.40625L1.42188 13.9844L0.015625 12.5781L5.59375 7L0.015625 1.42188L1.42188 0.015625L7 5.59375L12.5781 0.015625L13.9844 1.42188Z" fill="#666666"/>
+            </svg>
+        </div>
+    );
+};
