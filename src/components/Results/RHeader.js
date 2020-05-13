c9a33b908363fa3cf1a7301f4a5beabe10b8c171
avtoriscenaria <fafterty@gmail.com>
2020-05-13 12:36:25 +0200

    add time zone format

--- src/components/Results/RHeader.js
+++ src/components/Results/RHeader.js
@@ -4,12 +4,21 @@ import "./RHeader.scss";
 import {formatDate, formatTime} from "../../utils/timeFormat";
 
 class RHeader extends React.Component {
+
+    makeFormat = (date) => {
+      if (date) {
+          let n = new Date().getTimezoneOffset();
+
+          return date + (n * 3600 * 1000)/-60
+      }
+    };
+
     render() {
         const {user} = this.props;
         return <div className="r-header">
             <div className="u-name">{user.name}</div>
             <div className="u-email">{user.email}</div>
-            <div className="u-date">{`${t.date} - ${formatDate(user.end_time)}`}</div>
+            <div className="u-date">{`${t.date} - ${formatDate(this.makeFormat(user.end_date) || user.end_time)}`}</div>
             <div className="u-time">{`${t.all_time_test} - ${formatTime(user.end_time - user.start_time)}`}</div>
         </div>
     }
