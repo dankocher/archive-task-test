c3f75616329b737e3df19f0fd97d0a8f021eb0a3
karachunvladislav <karachunvladislav@gmail.com>
2020-12-28 13:57:46 +0300

    fix: fix height textarea

--- src/views/ArtTest/Components/TextArea/TextArea.js
+++ src/views/ArtTest/Components/TextArea/TextArea.js
@@ -7,64 +7,70 @@ import { isFunction } from "../../utils/validators/isFunction";
 const classNames = require("classnames");
 
 function TextArea(props) {
-	const {
-		rows,
-		defaultValue = "",
-		text,
-		setText = () => {},
-		defaultHeight,
-		error,
-		...args
-	} = props;
+  const {
+    rows,
+    defaultValue = "",
+    text,
+    setText = () => {},
+    defaultHeight,
+    error,
+    ...args
+  } = props;
 
-	const textAreaRef = useRef(null);
-	// const [text, setText] = useState("");
-	const [textAreaHeight, setTextAreaHeight] = useState("auto");
+  const textAreaRef = useRef(null);
+  // const [text, setText] = useState("");
+  const [textAreaHeight, setTextAreaHeight] = useState("auto");
 
-	const maxLength = props.maxLength ? props.maxLength : 240;
-	const onBlur = isFunction(props.onBlur)
-		? props.onBlur
-		: () => console.log("Is not a function");
+  const maxLength = props.maxLength ? props.maxLength : 240;
+  const onBlur = isFunction(props.onBlur)
+    ? props.onBlur
+    : () => console.log("Is not a function");
 
-	useEffect(() => {
-		if (defaultValue === text) return;
-		setTextAreaHeight("auto");
-		setText(defaultValue);
-	}, [defaultValue]);
+  useEffect(() => {
+    if (defaultValue === text) return;
+    setTextAreaHeight("auto");
+    setText(defaultValue);
+  }, [defaultValue]);
 
-	useEffect(() => {
-		// console.log(textAreaRef.current.scrollHeight);
-		setTextAreaHeight(`${textAreaRef.current.scrollHeight}px`);
-	}, [text]);
+  useEffect(() => {
+    // console.log(textAreaRef.current.scrollHeight);
+    setTextAreaHeight(`${textAreaRef.current.scrollHeight}px`);
+  }, [text]);
 
-	const onChangeHandler = (event) => {
-		const text = event.target.value;
-		if (text.length >= maxLength) {
-			return;
-		}
-		setTextAreaHeight("auto");
-		setText(event.target.value);
-	};
+  const onChangeHandler = (event) => {
+    const text = event.target.value;
+    if (text.length >= maxLength) {
+      return;
+    }
+    setTextAreaHeight("auto");
+    setText(event.target.value);
+  };
 
-	const defaultStyles = classNames(styles.textField, {
-		[styles.error]: error,
-	});
+  const defaultStyles = classNames(styles.textField, {
+    [styles.error]: error,
+  });
+  useEffect(() => {
+    console.log(textAreaHeight.replace("px", ""));
+  });
 
-	return (
-		<textarea
-			{...args}
-			ref={textAreaRef}
-			rows={rows ? rows : 1}
-			style={{
-				height: textAreaHeight,
-				overflow: "hidden",
-			}}
-			value={text}
-			className={props.className != null ? props.className : defaultStyles}
-			onChange={onChangeHandler}
-			onBlur={() => onBlur(text)}
-		/>
-	);
+  return (
+    <textarea
+      {...args}
+      ref={textAreaRef}
+      rows={rows ? rows : 1}
+      style={{
+        height:
+          parseInt(textAreaHeight.replace("px", "")) < 26
+            ? "26px"
+            : textAreaHeight,
+        overflow: "hidden",
+      }}
+      value={text}
+      className={props.className != null ? props.className : defaultStyles}
+      onChange={onChangeHandler}
+      onBlur={() => onBlur(text)}
+    />
+  );
 }
 
 export default TextArea;
