713c2f5081bc9e27ebd508ad65d9793827bcb61f
karachunvladislav <karachunvladislav@gmail.com>
2021-02-08 14:28:03 +0300

    fix: text error Display

--- src/views/ArtTest/Components/DeviceError/DeviceError.js
+++ src/views/ArtTest/Components/DeviceError/DeviceError.js
@@ -2,17 +2,18 @@ import React from "react";
 import styles from "./deviceError.module.scss";
 
 function DeviceError() {
-	return (
-		<div className={styles.container}>
-			<div>
-				<h1>Oops!</h1>
-				<p>
-					Сайт не поддерживает ваше устройство. Попробуйте зайти с другого
-					устройства.
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>Oops!</h1>
+        <p>
+          У вашего устройства слишком маленькой разрешение, воспользуйтесь
+          ноутбуком или компьютером. Сайт работает на устройствах с разрешением
+          выше 1365 PX
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default DeviceError;
